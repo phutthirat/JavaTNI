fca13771f72dedf7dcb756bd98d2143f6d8b011c
phutthirat <155803218+phutthirat@users.noreply.github.com>
2026-01-26 23:37:52 +0700

    2513510012 sumitted Lab9

---
+++ Movie.java
@@ -0,0 +1,23 @@
+public class Movie {
+    private String title;
+    private int minutes;
+    private String rating;
+
+    public Movie(String title, int minutes, String rating) {
+        this.title = title;
+        this.minutes = minutes;
+        this.rating = rating;
+    }
+
+    public Movie() {
+        this("", 0, "");
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String toString() {
+        return title + " (" + rating + ", " + minutes + " mins)";
+    }
+}
---
+++ TicketInfo.java
@@ -0,0 +1,35 @@
+import javax.swing.JOptionPane;
+
+public class TicketInfo {
+
+    public static String select_showtime(Movie movie) {
+        String menu = movie.toString() +
+                "\nPress 1 to select show time = 13:00" +
+                "\nPress 2 to select show time = 14:30" +
+                "\nPress 3 to select show time = 15:00" +
+                "\nEnter a number:";
+        String choice = JOptionPane.showInputDialog(null, menu);
+
+        if ("1".equals(choice)) return "13:00";
+        if ("2".equals(choice)) return "14:30";
+        if ("3".equals(choice)) return "15:00";
+        return "Error time";
+    }
+
+    public static String select_seat_number() {
+        String row = JOptionPane.showInputDialog(null, "Select seat row [A-G]:");
+        String number = JOptionPane.showInputDialog(null, "Select seat number [1-12]:");
+        if (row != null && number != null) {
+            return row.toUpperCase() + number;
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Movie movie = new Movie("Titanic", 120, "PG-13");
+        String show_time = select_showtime(movie);
+        String seat_number = select_seat_number();
+        Ticket ticket = new Ticket("T001", movie, show_time, seat_number, 240.0);
+        JOptionPane.showMessageDialog(null, ticket.toString());
+    }
+}
